42ca096c992310889f29d2d0ea7eea99fc01b5e2
dragon-ninja <129362302+dragon-ninja@users.noreply.github.com>
2024-03-15 16:48:45 +0530

    commit

--- assest/js/app.js
+++ assest/js/app.js
@@ -1,16 +1,61 @@
+// Hamburger menu functionality
+const ham = document.querySelector('.ham');
+const nav = document.querySelector('.nav_menu');
 
-let nextDom = document.getElementById("next");
-let prevDom = document.getElementById("prev");
+ham.addEventListener('click', () => {
+  ham.classList.toggle('transform');
+  nav.classList.toggle('mobile');
+});
 
-let carouselDom = document.querySelector(".carousel");
-let SliderDom = carouselDom.querySelector(".carousel .list");
-let thumbnailBorderDom = document.querySelector(".carousel .thumbnail");
-let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll(".item");
-let timeDom = document.querySelector(".carousel .time");
+// Sticky header on scroll
+const topHeader = document.querySelector('.Header_bg_color');
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY <= 100) {
+    topHeader.classList.remove('sticky');
+  } else {
+    topHeader.classList.add('sticky');
+  }
+});
+
+// Tab functionality
+const tabs = document.querySelectorAll('.tab_contant');
+const tabBtns = document.querySelectorAll('.tabBtn li');
+
+tabBtns.forEach((tabBtn, index) => {
+  tabBtn.addEventListener('click', () => {
+    tabBtns.forEach(otherActive => {
+      otherActive.classList.remove('active');
+    });
+    tabBtn.classList.add('active');
+
+    tabs.forEach(tabClass => {
+      tabClass.classList.remove('active');
+    });
+
+    tabs[index].classList.add('active');
+  });
+});
+
+tabBtns[0].click(); // Activate the first tab initially
+
+// Carousel functionality
+const nextDom = document.getElementById("next");
+const prevDom = document.getElementById("prev");
+const carouselDom = document.querySelector(".carousel");
+const SliderDom = carouselDom.querySelector(".carousel .list");
+const thumbnailBorderDom = document.querySelector(".carousel .thumbnail");
+const thumbnailItemsDom = thumbnailBorderDom.querySelectorAll(".item");
+const timeDom = document.querySelector(".carousel .time");
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+
 let timeRunning = 5000;
 let timeAutoNext = 8000;
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+  next.click();
+}, timeAutoNext);
 
 nextDom.onclick = function () {
   showSlider("next");
@@ -19,15 +64,10 @@ nextDom.onclick = function () {
 prevDom.onclick = function () {
   showSlider("prev");
 };
-let runTimeOut;
-let runNextAuto = setTimeout(() => {
-  next.click();
-}, timeAutoNext);
+
 function showSlider(type) {
   let SliderItemsDom = SliderDom.querySelectorAll(".carousel .list .item");
-  let thumbnailItemsDom = document.querySelectorAll(
-    ".carousel .thumbnail .item"
-  );
+  let thumbnailItemsDom = document.querySelectorAll(".carousel .thumbnail .item");
 
   if (type === "next") {
     SliderDom.appendChild(SliderItemsDom[0]);
@@ -50,53 +90,9 @@ function showSlider(type) {
   }, timeAutoNext);
 }
 
-
-
-var ham =document.querySelector('.ham');
-var nav =document.querySelector('.nav_menu')
-
-ham.addEventListener('click',()=>{
-  ham.classList.toggle('transform');
-  nav.classList.toggle('mobile');
-})
-var topHeader =document.querySelector('.Header_bg_color');
-
-window.addEventListener('scroll', function(){
-  if (window.scrollY <= 100) {
-    topHeader.classList.remove('sticky');
-  }
-  else{
-    topHeader.classList.add('sticky');
-  }
-   
-});
-
-
-const tabs = document.querySelectorAll('.tab_contant');
-
-const tabBtns = document.querySelectorAll('.tabBtn li');
-
-tabBtns.forEach((tabBtn, index) => {
-  tabBtn.addEventListener('click', () => {
-    tabBtns.forEach(otherActive => {
-      otherActive.classList.remove('active');
-    });
-    tabBtn.classList.add('active');
-
-    tabs.forEach(tabClass => {
-      tabClass.classList.remove('active');
-    });
-
-    tabs[index].classList.add('active');
-  });
-});
-
-
+// Loader functionality
 const loader = document.querySelector('.loader');
-console.log(loader);
 
 window.addEventListener("load", (event) => {
-  loader.style.display="none";
+  loader.style.display = "none";
 });
-tabBtns[0].click();
-
