08f6a36b3a6c8cb727f8d4c81cd67ce44a790ede
dragon-ninja <129362302+dragon-ninja@users.noreply.github.com>
2024-03-16 15:56:15 +0530

    update

--- assest/js/app.js
+++ assest/js/app.js
@@ -1,3 +1,4 @@
+
 // Hamburger menu functionality
 const ham = document.querySelector('.ham');
 const nav = document.querySelector('.nav_menu');
@@ -6,6 +7,7 @@ ham.addEventListener('click', () => {
   ham.classList.toggle('transform');
   nav.classList.toggle('mobile');
 });
+
 
 // Sticky header on scroll
 const topHeader = document.querySelector('.Header_bg_color');
@@ -19,8 +21,31 @@ window.addEventListener('scroll', function () {
 });
 
 // Tab functionality
+
+//slider
+const slider = document.querySelector(".slider");
+function activate(e) {
+  const items = document.querySelectorAll(".item");
+  e.target.matches(".next") && slider.append(items[0]);
+  e.target.matches(".prev") && slider.prepend(items[items.length - 1]);
+}
+function autoSlide() {
+  const nextButton = document.querySelector(".next");
+  nextButton.click(); 
+}
+//setInterval(autoSlide, 5000);
+document.addEventListener("click", activate, false);
+
+
+// Loader functionality
+const loader = document.querySelector('.loader');
+
+window.addEventListener("load", (event) => {
+  loader.style.display = "none";
+});
 const tabs = document.querySelectorAll('.tab_contant');
 const tabBtns = document.querySelectorAll('.tabBtn li');
+
 
 tabBtns.forEach((tabBtn, index) => {
   tabBtn.addEventListener('click', () => {
@@ -35,64 +60,9 @@ tabBtns.forEach((tabBtn, index) => {
 
     tabs[index].classList.add('active');
   });
-});
 
-tabBtns[0].click(); // Activate the first tab initially
-
-// Carousel functionality
-const nextDom = document.getElementById("next");
-const prevDom = document.getElementById("prev");
-const carouselDom = document.querySelector(".carousel");
-const SliderDom = carouselDom.querySelector(".carousel .list");
-const thumbnailBorderDom = document.querySelector(".carousel .thumbnail");
-const thumbnailItemsDom = thumbnailBorderDom.querySelectorAll(".item");
-const timeDom = document.querySelector(".carousel .time");
-
-thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-
-let timeRunning = 5000;
-let timeAutoNext = 8000;
-let runTimeOut;
-let runNextAuto = setTimeout(() => {
-  next.click();
-}, timeAutoNext);
-
-nextDom.onclick = function () {
-  showSlider("next");
-};
-
-prevDom.onclick = function () {
-  showSlider("prev");
-};
-
-function showSlider(type) {
-  let SliderItemsDom = SliderDom.querySelectorAll(".carousel .list .item");
-  let thumbnailItemsDom = document.querySelectorAll(".carousel .thumbnail .item");
-
-  if (type === "next") {
-    SliderDom.appendChild(SliderItemsDom[0]);
-    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-    carouselDom.classList.add("next");
-  } else {
-    SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-    thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-    carouselDom.classList.add("prev");
+  // Trigger click event only for the first button
+  if (index === 0) {
+    tabBtn.click();
   }
-  clearTimeout(runTimeOut);
-  runTimeOut = setTimeout(() => {
-    carouselDom.classList.remove("next");
-    carouselDom.classList.remove("prev");
-  }, timeRunning);
-
-  clearTimeout(runNextAuto);
-  runNextAuto = setTimeout(() => {
-    next.click();
-  }, timeAutoNext);
-}
-
-// Loader functionality
-const loader = document.querySelector('.loader');
-
-window.addEventListener("load", (event) => {
-  loader.style.display = "none";
 });
